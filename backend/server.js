71e598b400858b6504ba8833a85bf1bb32c6d1ab
rushilpatel21 <rushilpatel210@gmail.com>
2024-03-23 17:57:31 +0530

    Change port number for server to process.env.PORT or 8000

--- backend/server.js
+++ backend/server.js
@@ -240,6 +240,6 @@ app.get("/api/foodlog", async (req, res) => {
     }
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+app.listen(process.env.PORT || 8000, () => {
+  console.log("Server started on port " + (process.env.PORT || 8000));
 });
