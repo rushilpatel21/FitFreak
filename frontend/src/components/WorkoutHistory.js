b41f9cb3e5a53f5f921ba73e0b4cef79bbe7b7ce
rushilpatel21 <rushilpatel210@gmail.com>
2024-04-01 14:21:59 +0530

    "Added API calls to fetch workout data for user, updated doughnut graph to display workout types for today and past month"

--- frontend/src/components/WorkoutHistory.js
+++ frontend/src/components/WorkoutHistory.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 // import Notification from './Notification.js';
 import { Bar, Doughnut } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
+import axios from 'axios';
 import Swal from 'sweetalert2'
 
 function WorkoutHistory() {
@@ -16,103 +17,247 @@ function WorkoutHistory() {
   const [workoutTypeDayData,setWorkoutTypeDayData] = useState({});
   const [workoutTypeMonthData,setWorkoutTypeMonthData] = useState({});
   const [displayModeType, setDisplayModeType] = useState('Days');
+  const [userName, setUserName] = useState('');
 
   useEffect(() => {
     const userState = localStorage.getItem('isLoggedIn');
     if (userState === 'false') {
       setShowNotification(true);
+    } else {
+      const storedUser = JSON.parse(localStorage.getItem('userDetail'));
+      setUserName(storedUser.username);
     }
   }, []);
 
   useEffect(() => { //This is for bar graph as it will have total workout
     
-    const MockWorkoutDataForDays = {
-      labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7'],
-      datasets: [
-        {
-          label: 'Workout Minutes',
-          data: [30, 45, 60, 40, 50, 20, 80],
-          backgroundColor: [
-            'rgb(83,124,56)',
-            'rgb(123,165,145)',
-            'rgb(204, 34, 43)',
-            'rgb(241, 91, 76)',
-            'rgb(250, 164, 27)',
-            'rgb(255,212,91)',
-            'rgb(255,229,180)'
-          ],
-          // backgroundColor: 'rgba(54, 162, 235, 0.6)',
-          borderColor: 'rgba(54, 162, 235, 1)',
-          borderWidth: 1,
-        },
-      ],
-    };
-    const MockWorkoutDataForMonths = {
-      labels: ['Month 1', 'Month 2', 'Month 3', 'Month 4', 'Month 5', 'Month 6'],
-      datasets: [
-        {
-          label: 'Workout Minutes',
-          data: [45, 30, 90, 20, 70,110], 
-          backgroundColor: [
-            'rgb(83,124,56)',
-            'rgb(123,165,145)',
-            'rgb(204, 34, 43)',
-            'rgb(241, 91, 76)',
-            'rgb(250, 164, 27)',
-            'rgb(255,212,91)'
-          ],
-          borderColor: 'rgba(54, 162, 235, 1)',
-          borderWidth: 1,
-        },
-      ],
-    };
-    setWorkoutDayData(MockWorkoutDataForDays);
-    setWorkoutMonthData(MockWorkoutDataForMonths);
-    console.log(workoutDayData);
-  }, [workoutDayData]);
+    const fetchData = async () => {
+      try {
+        const storedUser = JSON.parse(localStorage.getItem('userDetail'));
+        const userId = storedUser.username;
+        if (!userId) {
+          return;
+        }
 
+        const response = await axios.get(`/api/workoutlog/${userId}`);
 
-  useEffect(() => { //This is for doughnut graph as it will have types of workout
+        if (response.status === 200) {
+          const fetchedWorkoutData = response.data.workoutlog;
+
+          const today = new Date();
+          const pastSevenDaysData = fetchedWorkoutData.filter(data => {
+            const workoutDate = new Date(data.workoutDate);
+            return workoutDate > new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000);
+          });
+          const workoutMinutesByDay = {};
+          pastSevenDaysData.forEach(data => {
+            const workoutDate = data.workoutDate;
+            const workoutMinutes = parseInt(data.workoutMinutes);
+            if (workoutMinutesByDay[workoutDate]) {
+              workoutMinutesByDay[workoutDate] += workoutMinutes;
+            } else {
+              workoutMinutesByDay[workoutDate] = workoutMinutes;
+            }
+          });
+          setWorkoutDayData({
+            labels: Object.keys(workoutMinutesByDay).map(date => new Date(date).getDate() + ' ' + new Date(date).toLocaleString('default', { month: 'long' }) + ' ' + new Date(date).getFullYear()),
+            datasets: [{
+              label: 'Workout Minutes',
+              data: Object.values(workoutMinutesByDay),
+              backgroundColor: [
+                'rgb(83,124,56)',
+                'rgb(123,165,145)',
+                'rgb(204, 34, 43)',
+                'rgb(241, 91, 76)',
+                'rgb(250, 164, 27)',
+                'rgb(255,212,91)',
+                'rgb(255,229,180)'
+              ],
+              borderColor: 'rgba(54, 162, 235, 1)',
+              borderWidth: 1,
+            }],
+          });
+          const pastSixMonthsData = fetchedWorkoutData.filter(data => {
+            const workoutDate = new Date(data.workoutDate);
+            const sixMonthsAgo = new Date(today.getFullYear(), today.getMonth() - 6, today.getDate());
+            return workoutDate > sixMonthsAgo;
+          });
+          const workoutMinutesByMonth = {};
+          pastSixMonthsData.forEach(data => {
+            const workoutMonth = new Date(data.workoutDate).toLocaleString('default', { month: 'long' }) + ' ' + new Date(data.workoutDate).getFullYear();
+            const workoutMinutes = parseInt(data.workoutMinutes);
+            if (workoutMinutesByMonth[workoutMonth]) {
+              workoutMinutesByMonth[workoutMonth] += workoutMinutes;
+            } else {
+              workoutMinutesByMonth[workoutMonth] = workoutMinutes;
+            }
+          });
+          setWorkoutMonthData({
+            labels: Object.keys(workoutMinutesByMonth),
+            datasets: [{
+              label: 'Workout Minutes',
+              data: Object.values(workoutMinutesByMonth),
+              backgroundColor: [
+                'rgb(83,124,56)',
+                'rgb(123,165,145)',
+                'rgb(204, 34, 43)',
+                'rgb(241, 91, 76)',
+                'rgb(250, 164, 27)',
+                'rgb(255,212,91)'
+              ],
+              borderColor: 'rgba(255, 99, 132, 1)',
+              borderWidth: 1,
+            }],
+          });
+        }
+      } catch (error) {
+        console.error("Error fetching workout data:", error);
+      }
+    };
     
-    const MockWorkoutDataForDays = { // One type would for for today, different types of workout
-      labels: ['Cardio', 'Strength Training', 'Flexibility Training', 'Balance & Stability'],
-      datasets: [
-        {
-          label: 'Workout Minutes',
-          data: [30, 45, 60, 40],
-          backgroundColor: [
-            'rgb(83,124,56)',
-            'rgb(123,165,145)',
-            'rgb(204, 34, 43)',
-            'rgb(241, 91, 76)'
-          ],
-          borderColor: 'rgba(54, 162, 235, 1)',
-          borderWidth: 1,
-        },
-      ],
-    };
-    const MockWorkoutDataForMonths = { // this for the past 7 days, different types of workout
-      labels: ['Cardio', 'Strength Training', 'Flexibility Training', 'Balance & Stability'],
-      datasets: [
-        {
-          label: 'Workout Minutes',
-          data: [450, 300, 500, 460], 
-          backgroundColor: [
-            'rgb(83,124,56)',
-            'rgb(123,165,145)',
-            'rgb(204, 34, 43)',
-            'rgb(241, 91, 76)'
-          ],
-          borderColor: 'rgba(54, 162, 235, 1)',
-          borderWidth: 1,
-        },
-      ],
+    fetchData();
+  }, []);
+
+
+  // useEffect(() => { //This is for doughnut graph as it will have types of workout
+    
+  //   const MockWorkoutDataForDays = { // One type would be for today, different types of workout
+  //     labels: ['Cardio', 'Strength Training', 'Flexibility Training', 'Balance & Stability'],
+  //     datasets: [
+  //       {
+  //         label: 'Workout Minutes',
+  //         data: [30, 45, 60, 40],
+  //         backgroundColor: [
+  //           'rgb(83,124,56)',
+  //           'rgb(123,165,145)',
+  //           'rgb(204, 34, 43)',
+  //           'rgb(241, 91, 76)'
+  //         ],
+  //         borderColor: 'rgba(54, 162, 235, 1)',
+  //         borderWidth: 1,
+  //       },
+  //     ],
+  //   };
+  //   const MockWorkoutDataForMonths = { // this for the past 7 days, different types of workout
+  //     labels: ['Cardio', 'Strength Training', 'Flexibility Training', 'Balance & Stability'],
+  //     datasets: [
+  //       {
+  //         label: 'Workout Minutes',
+  //         data: [450, 300, 500, 460], 
+  //         backgroundColor: [
+  //           'rgb(83,124,56)',
+  //           'rgb(123,165,145)',
+  //           'rgb(204, 34, 43)',
+  //           'rgb(241, 91, 76)'
+  //         ],
+  //         borderColor: 'rgba(54, 162, 235, 1)',
+  //         borderWidth: 1,
+  //       },
+  //     ],
+  //   };
+
+  //   setWorkoutTypeDayData(MockWorkoutDataForDays);
+  //   setWorkoutTypeMonthData(MockWorkoutDataForMonths);
+  //   console.log(workoutDayData);
+  // }, []);
+
+  useEffect(() => { // This is for doughnut graph as it will have types of workout
+
+    const fetchData = async () => {
+      try {
+        const storedUser = JSON.parse(localStorage.getItem('userDetail'));
+        const userId = storedUser.username;
+        if (!userId) {
+          return;
+        }
+
+        const response = await axios.get(`/api/workoutlog/${userId}`);
+
+        if (response.status === 200) {
+          const fetchedWorkoutData = response.data.workoutlog;
+
+          // Filter data for today
+          const today = new Date().toISOString().slice(0, 10);
+          const todayWorkouts = fetchedWorkoutData.filter(data => data.workoutDate === today);
+
+          // Aggregate workout minutes by type for today
+          const todayWorkoutMinutesByType = {};
+          todayWorkouts.forEach(data => {
+            const workoutType = data.workoutType;
+            const workoutMinutes = parseInt(data.workoutMinutes);
+            if (todayWorkoutMinutesByType[workoutType]) {
+              todayWorkoutMinutesByType[workoutType] += workoutMinutes;
+            } else {
+              todayWorkoutMinutesByType[workoutType] = workoutMinutes;
+            }
+          });
+
+          // Prepare data for today's doughnut chart
+          const todayWorkoutData = {
+            labels: Object.keys(todayWorkoutMinutesByType),
+            datasets: [{
+              label: 'Workout Minutes',
+              data: Object.values(todayWorkoutMinutesByType),
+              backgroundColor: [
+                'rgb(83,124,56)',
+                'rgb(123,165,145)',
+                'rgb(204, 34, 43)',
+                'rgb(241, 91, 76)'
+              ],
+              borderColor: 'rgba(54, 162, 235, 1)',
+              borderWidth: 1,
+            }],
+          };
+
+          // Filter data for the past month
+          const pastMonthData = fetchedWorkoutData.filter(data => {
+            const workoutDate = new Date(data.workoutDate);
+            const oneMonthAgo = new Date();
+            oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
+            return workoutDate > oneMonthAgo;
+          });
+
+          // Aggregate workout minutes by type for the past month
+          const pastMonthWorkoutMinutesByType = {};
+          pastMonthData.forEach(data => {
+            const workoutType = data.workoutType;
+            const workoutMinutes = parseInt(data.workoutMinutes);
+            if (pastMonthWorkoutMinutesByType[workoutType]) {
+              pastMonthWorkoutMinutesByType[workoutType] += workoutMinutes;
+            } else {
+              pastMonthWorkoutMinutesByType[workoutType] = workoutMinutes;
+            }
+          });
+
+          // Prepare data for the past month's doughnut chart
+          const pastMonthWorkoutData = {
+            labels: Object.keys(pastMonthWorkoutMinutesByType),
+            datasets: [{
+              label: 'Workout Minutes',
+              data: Object.values(pastMonthWorkoutMinutesByType),
+              backgroundColor: [
+                'rgb(83,124,56)',
+                'rgb(123,165,145)',
+                'rgb(204, 34, 43)',
+                'rgb(241, 91, 76)'
+              ],
+              borderColor: 'rgba(54, 162, 235, 1)',
+              borderWidth: 1,
+            }],
+          };
+
+          // Set state with the fetched data
+          setWorkoutTypeDayData(todayWorkoutData);
+          setWorkoutTypeMonthData(pastMonthWorkoutData);
+        }
+      } catch (error) {
+        console.error("Error fetching workout data:", error);
+      }
     };
 
-    setWorkoutTypeDayData(MockWorkoutDataForDays);
-    setWorkoutTypeMonthData(MockWorkoutDataForMonths);
-    console.log(workoutDayData);
-  }, [workoutDayData]);
+    fetchData();
+  }, []);
+
 
 
 
